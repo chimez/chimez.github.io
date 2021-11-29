5aae4a6e09c8dd13e1ef2eaf278e6fbe3dba9701
Peigeng Zhong <zhongpg@outlook.com>
2021-11-29 14:30:13 +0800

    完善安装步骤，改用 venv

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/bash
 
-mamba install "Nikola[extras]"
+python3 -m venv nikola 
+source nikola/bin/activate
+python -m pip install -U pip setuptools wheel
+python -m pip install -U "Nikola[extras]"
 nikola plugin -i orgmode
